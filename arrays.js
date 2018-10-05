fff18f341788b5d843012ec0b701c6ba057d270d
Zoey Cluff <zoey.cluff@gmail.com>
2018-10-05 10:27:14 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -19,7 +19,7 @@ function addElementToEndOfArray(array, element) {
 
 function destructivelyAddElementToEndOfArray(array, element) {
   
-  return array.shift(element)
+  return array.push(element)
 }
 
 function accessElementInArray(array, index) {

f4d4c8b3344b3d876f02ce41cf9e0353b8701338
Zoey Cluff <zoey.cluff@gmail.com>
2018-10-05 10:24:30 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -6,9 +6,8 @@ function addElementToBeginningOfArray(array, element){
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
-  array.unshift(element)
-  return array
-}
+  
+  return array.unshift(element)
 
 function addElementToEndOfArray(array, element) {
   var changed = [array, ...element];
